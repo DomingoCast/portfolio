76db31e243bab325097278e7c54e8e770242fe40
sortzgandea <castillomartidomingo@gmail.com>
2020-08-13 08:59:09 +0000

    added personal info in contact page

--- portfolio-react/src/components/Contact/Contact.js
+++ portfolio-react/src/components/Contact/Contact.js
@@ -10,7 +10,12 @@ const contact = (props) => {
             <div className={classes.linkContainer}>
                 <a target="_blank" rel="noopener noreferrer"  href="https://www.linkedin.com/in/domingo-castillo-342b8b17a" className={classes.link}>linkedin</a>
                 <a target="_blank" rel="noopener noreferrer"  href="https://github.com/DomingoCast" className={classes.link}>github</a>
-                <a target="_blank" rel="noopener noreferrer"  href="mailto:castillomartidomingo@gmail.com" className={classes.link}>email</a>
+                <a target="_blank" rel="noopener noreferrer"  href="https://www.instagram.com/domingo_cast/" className={classes.link}>instagram</a>
+                <a target="_blank" rel="noopener noreferrer"  href="https://www.youtube.com/user/castillomartidomingo" className={classes.link}>youtube</a>
+            </div>
+            <div className={classes.info}>
+                <span className={classes.text}>tlf: +34 688 90 15 07</span><br/><br/>
+                <span className={classes.text}>email: castillomartidomingo@gmail.com</span>
             </div>
         </div>
         <div className={classes.border}></div>
